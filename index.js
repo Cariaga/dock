bc4410caede0a5623a951590da87a9603cb73d4d
Kaivaan <cariaga.info@gmail.com>
2020-04-30 08:07:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello!')
+  res.send('Hello test!')
 })
 //Launch listening server on port 8081
 app.listen(8081, function () {
